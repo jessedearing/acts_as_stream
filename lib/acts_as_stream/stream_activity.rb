a5898dd68e7400328be3d8dd95cfbd2df650cba3
John Metta <john@johnmetta.com>
2012-03-12 15:01:07 -0700

    Clean up specs

--- lib/acts_as_stream/stream_activity.rb
+++ lib/acts_as_stream/stream_activity.rb
@@ -4,27 +4,27 @@ module ActsAsStream
     def package options = {}
       options.assert_valid_keys(:who, :action, :time, :object, :ignore_stream_hash_on)
       raise "You need at least a :who and an :action! to create an activity package" if options[:who].nil? or options[:action].nil?
-      options = {:time => Time.now.to_i, :ignore_stream_hash_on => []}.merge options
+      opts = {:time => Time.now.to_i, :ignore_stream_hash_on => []}.merge options
       # Try to ensure :time is in seconds
-      options[:time] = options[:time].to_i if options[:time].is_a?(Time)
+      opts[:time] = opts[:time].to_i if opts[:time].is_a?(Time)
 
       # If Objects provide the :to_stream_hash method, use it.
-      if options[:ignore_stream_hash_on].present?
+      if opts[:ignore_stream_hash_on].present?
         #convenience, make sure it's an array so we can use include? instead of "include? or equals"
-        options[:ignore_stream_hash_on] = [options[:ignore_stream_hash_on]] unless options[:ignore_stream_hash_on].is_a?(Array)
+        opts[:ignore_stream_hash_on] = [opts[:ignore_stream_hash_on]] unless opts[:ignore_stream_hash_on].is_a?(Array)
       end
 
       [:who, :object].each do |opt|
         #unless we are ignoring the stream hash for this object, use StreamableObject.stream_hash
-        unless options[:ignore_stream_hash_on].include?(opt)
-          options[opt] = options[opt].to_stream_hash
+        unless opts[:ignore_stream_hash_on].include?(opt)
+          opts[opt] = opts[opt].to_stream_hash
         end
       end
 
-      options.delete(:ignore_stream_hash_on)
+      opts.delete(:ignore_stream_hash_on)
 
       # then, if everything is fine, bundle it up into a JSON string
-      options.to_json
+      opts.to_json
     end
   end
 end
--- spec/stream_activity_spec.rb
+++ spec/stream_activity_spec.rb
@@ -16,7 +16,7 @@ describe ActsAsStream::StreamActivity do
 
   describe "time" do
     it "should create a valid package with all valid options" do
-      ActsAsStream.package(@valid_options).should == @valid_json
+      package(@valid_options).should == @valid_json
     end
 
     it "should create a valid package without a time" do
@@ -26,16 +26,16 @@ describe ActsAsStream::StreamActivity do
       time = Time.now.to_i
 
       json = "{\"time\":#{time},\"who\":{\"user\":{\"id\":#{@user.id}}},\"action\":\"Tested StreamActivity!\",\"object\":{\"widget\":{\"id\":#{@widget.id}}}}"
-      ActsAsStream.package(options).should == json
+      package(options).should == json
     end
 
     it "should create a valid package with a Time object" do
       options = @valid_options.dup
-      options.delete(:time)
-      #Making an assumption this won't block on time!
       time = Time.now
+      options[:time] = time
+      #Making an assumption this won't block on time!
       json = "{\"time\":#{time.to_i},\"who\":{\"user\":{\"id\":#{@user.id}}},\"action\":\"Tested StreamActivity!\",\"object\":{\"widget\":{\"id\":#{@widget.id}}}}"
-      ActsAsStream.package(options).should == json
+      decode(package(options)).should == decode(json)
     end
   end
 
@@ -43,12 +43,36 @@ describe ActsAsStream::StreamActivity do
     it "should encode the actor as complete json" do
       options = @valid_options.dup
       options[:ignore_stream_hash_on] = :who
-      ActsAsStream.package(options).should_not == @valid_json
+      hash = {:who => @user, :action => options[:action], :time => options[:time], :object => @widget.to_stream_hash}
+      pack = package(options)
+      pack.should_not == @valid_json
+      decode(pack).should == decode(hash.to_json)
     end
+    it "should encode the object as complete json" do
+      options = @valid_options.dup
+      options[:ignore_stream_hash_on] = :object
+      hash = {:who => @user.to_stream_hash, :action => options[:action], :time => options[:time], :object => @widget}
+      pack = package(options)
+      pack.should_not == @valid_json
+      decode(pack).should == decode(hash.to_json)
+    end
+
+    it "should encode the actor and object as complete json" do
+      options = @valid_options.dup
+      options[:ignore_stream_hash_on] = [:who, :object]
+      hash = {:who => @user, :action => options[:action], :time => options[:time], :object => @widget}
+      pack = package(options)
+      pack.should_not == @valid_json
+      decode(pack).should == decode(hash.to_json)
+    end
+
   end
   private
 
-  def test_package(options={})
+  def decode json
+    ActiveSupport::JSON.decode(json)
+  end
+  def package(options={})
     ActsAsStream.package options
   end
 end
